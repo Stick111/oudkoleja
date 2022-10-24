564fd18b7d28e83324e603bb36dce8f653c657c2
szpytka <jakub.szpytka@gmail.com>
2022-10-24 13:10:56 +0200

    policz na mnozenie

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@
 
 int policz(int a, int b){
 
-    return a + b;
+    return a * b;             ///zmiana na mnożenie
 }
 int main() {
 
     std::cout << "Hello, World!" << std::endl;
 
-    std::cout << "w pierwszym branchu; 2 + 3 = " <<policz(2,3) << std::endl;
+    std::cout << "w pierwszym branchu; 2 * 3 = " <<policz(2,3) << std::endl;
     return 0;
 }
