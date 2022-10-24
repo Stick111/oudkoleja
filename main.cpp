294f73d4cf1312a8901ccc7028c0b0e61383f67d
szpytka <jakub.szpytka@gmail.com>
2022-10-24 13:01:14 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+int policz(int a, int b){
+    return a + b;
+}
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "w pierwszym branchu; 2 + 3 = " <<policz(2,3) << std::endl;
+    return 0;
+}
